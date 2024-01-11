1b4ef8285dcea99340574db58629b675513da431
Perry Timinah <timnahwekalao@gmail.com>
2024-01-11 14:46:22 -0800

    Holberton School

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -2,4 +2,4 @@
 str = "Holberton School"
 first_str = "Holberton"
 second_str = "School"
-print(f"{first_str} {second_str}{first_str} {second_str}{first_str} {second_str}\n{first_str}\n")
+print(f"{first_str} {second_str}{first_str} {second_str}{first_str} {second_str}\n{first_str}")
