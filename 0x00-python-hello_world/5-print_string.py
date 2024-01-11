76f810615ceab79da8f11a7f8962d950dd9447ba
Perry Timinah <timnahwekalao@gmail.com>
2024-01-11 14:42:45 -0800

    Holberton School

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+first_str = "Holberton"
+second_str = "School"
+print(f"{first_str} {second_str}{first_str} {second_str}{first_str} {second_str}\n{first_str}")
