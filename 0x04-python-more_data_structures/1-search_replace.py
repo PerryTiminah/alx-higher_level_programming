69f1d7389c49f9e03b61d2f9d14d8064a91ba913
Perry Timinah <timnahwekalao@gmail.com>
2024-01-12 12:34:26 -0800

    print function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
 #!/usr/bim/python3
 def search_replace(my_list, search, replace):
-    return list(map(lambda e: replace if e == search else e, my_list))
+    new_list = list(map(lambda e: replace if e == search else e, my_list))
+    return (new_list)
