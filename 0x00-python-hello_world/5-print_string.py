ee62e4b45ccf35637cccf2896f0485862f392316
Perry Timinah <timnahwekalao@gmail.com>
2024-01-12 00:14:55 -0800

    printing multiples to the screen

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-first_str = "Holberton"
-second_str = "School"
-print(f"{first_str} {second_str}{first_str} {second_str}{first_str} {second_str}\n{first_str}")
+print(f"{str}{str}{str}")
+print(str[:9])
